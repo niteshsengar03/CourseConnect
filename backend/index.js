0ef3b4336430deebbf79f59a96d49ac3846eb733
Nitesh <niteshsengar9831@gmail.com>
2024-09-29 00:15:40 +0530

    Skeleton of app without routing fro admin and user

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,44 @@ const port = 3000;
 app.use(express.json());
 
 
-
-app.get('/', (req, res) => {
+// User
+app.post('user/signup', function (req, res){
     res.send('Hello World!');
 });
+
+app.post('user/login',function (req,res){
+    res.send("This is login router");
+})
+
+app.post('user/purchase-course',function(req,res){
+    res.send("Purchase the course");
+})
+
+app.get('user/course',function(req,res){
+    res.send("See all the course here");
+})
+
+
+//Admin
+app.post('admin/signup', function (req, res){
+    res.send('Hello World!');
+});
+
+app.post('admin/login',function (req,res){
+    res.send("This is login router");
+});
+
+app.post('admin/create-course',function (req,res){
+    res.send("Create a course")
+})
+
+app.delete('admin/course',function(req,res){
+    res.send("Deleting the course")
+})
+
+app.put('admin/update-cousrse',function(req,res){
+    res.send("Updating the course");
+})
 
 
 
