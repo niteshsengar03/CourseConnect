40f2a47d17e503c7f8d4972c807b5c0404111dd2
Nitesh <niteshsengar9831@gmail.com>
2024-09-29 14:32:09 +0530

    add db connection logic correctly

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ const {userRouter} = require('./routes/user');
 const {courseRouter} = require('./routes/courses');
 const { adminRouter } = require('./routes/admin');
 const mongoose = require('mongoose');
-const {userModel, adminModel, courseModel,purchaseModel} = require('./db/index');
+// const {userModel, adminModel, courseModel,purchaseModel} = require('./db/index');
+
 
 const app = express();
 const port = 3000;
@@ -17,12 +18,16 @@ app.use("ap1/v1/course",courseRouter);
 app.use("api/v1/admin",adminRouter)
 
 
-
 //Server will not start till the database is connected
 async function main(){
+try{
 await mongoose.connect("mongodb+srv://niteshsengar9831:oPNhUMViFEWtLWfV@cluster0.dtw65.mongodb.net/CourseConnect");
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+} catch (e) {
+    console.error("Failed to connect to the database");
 }
+}
+
 main();
